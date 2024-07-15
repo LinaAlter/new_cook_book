81299dc03cc12296573c37b2436108026e3f4c0c
Anna Volkova <anotherwaygirl@yandex.ru>
2024-07-15 20:40:30 +0700

    Initial commit

---
+++ new_cook_book.py
@@ -0,0 +1,20 @@
+import pprint
+
+with open ('receip.txt', encoding = 'utf-8') as file:
+    cook_book = {}
+    rec_list = []
+    dish = file.readline().strip()
+    ing_numb = file.readline()
+    n = int(ing_numb)
+    ing_list = file.readlines()[0:n]
+    
+    
+    for rec in ing_list:
+        rec= list(rec.split(' | '))
+        receip = {'ingredient_name': rec[0], 'quantity': rec[1], 'measure': rec[2].strip()}
+        if receip not in rec_list:
+            rec_list.append(receip)
+    cook_book[dish] = rec_list        
+    
+            
+pprint.pprint(cook_book)
