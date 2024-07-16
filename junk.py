ea582486266a812c5b95446f61f9c30ade3bfdb8
Anna Volkova <anotherwaygirl@yandex.ru>
2024-07-16 13:56:56 +0700

    all files changed

--- junk.py
+++ junk.py
@@ -2,25 +2,36 @@ import pprint
 
 with open ('receip.txt', encoding = 'utf-8') as file:
     cook_book = {}
-    rec_list = []
-    dish = file.readline().strip()
-    ing_numb = file.readline()
-    n = int(ing_numb)
-    ing_list = file.readlines()[0:n]
-    
     for line in file:
-        while True:
-            if line != " ":
-                for rec in ing_list:
-                    rec= list(rec.split(' | '))
-                    receip = {'ingredient_name': rec[0], 'quantity': rec[1], 'measure': rec[2].strip()}
-                if receip not in rec_list:
-                    rec_list.append(receip)
-                cook_book[dish] = rec_list        
-                
-            else:
-                break
-                
-pprint.pprint(cook_book)
+        dish = line.strip()
+        ing_numb = int(file.readline())
+        ing_dish = []
+        for r in range(ing_numb):
+            numb = file.readline().strip()
+            ingredient_name, quantity, measure = numb.split(' | ')
+            ing_dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+        cook_book[dish] = ing_dish
+        file.readline()
+        
+def get_shop_list_by_dishes(dishes, person_count):
+    
+    ings_shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+           for ing_dish in cook_book[dish]:
+               ings = int(ing_dish['quantity'])
+               ings *= person_count
+               ing_dish['quantity'] = ings
+               
+               ings_shop_list.setdefault(ing_dish['ingredient_name'], ing_dish )
+               
+               pprint.pprint(ings_shop_list)
 
-            
+   
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+         
+           
+    
+                 
--- new_cook_book.py
+++ new_cook_book.py
@@ -2,19 +2,14 @@ import pprint
 
 with open ('receip.txt', encoding = 'utf-8') as file:
     cook_book = {}
-    rec_list = []
-    dish = file.readline().strip()
-    ing_numb = file.readline()
-    n = int(ing_numb)
-    ing_list = file.readlines()[0:n]
-    
-    
-    for rec in ing_list:
-        rec= list(rec.split(' | '))
-        receip = {'ingredient_name': rec[0], 'quantity': rec[1], 'measure': rec[2].strip()}
-        if receip not in rec_list:
-            rec_list.append(receip)
-    cook_book[dish] = rec_list        
-    
-            
-pprint.pprint(cook_book)
+    for line in file:
+        dish = line.strip()
+        ing_numb = int(file.readline())
+        ing_dish = []
+        for r in range(ing_numb):
+            numb = file.readline().strip()
+            ingredient_name, quantity, measure = numb.split(' | ')
+            ing_dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+        cook_book[dish] = ing_dish
+        file.readline()
+
