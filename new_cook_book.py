fd0acd50dba2a87d0c0142e944da41d984e97739
Anna Volkova <anotherwaygirl@yandex.ru>
2024-07-16 18:37:39 +0700

    cook_book changed

--- new_cook_book.py
+++ new_cook_book.py
@@ -1,5 +1,20 @@
 import pprint
-
+def get_shop_list_by_dishes(dishes, person_count):
+    
+    ings_shop_list = {}
+    for dish in dishes:
+        for ing_dish in cook_book[dish]:
+            if dish in cook_book:
+           #for ing_dish in cook_book[dish]:
+                prod = ing_dish['ingredient_name']
+                ings_shop_list.setdefault(prod, [])
+                ings = int(ing_dish['quantity']) * person_count
+                ing_list = []
+                ing_list.append({'quantity': ings, 'measure': ing_dish['measure']})
+               
+                ings_shop_list[prod] = ing_list
+    pprint.pprint(ings_shop_list)
+    return ings_shop_list
 with open ('receip.txt', encoding = 'utf-8') as file:
     cook_book = {}
     for line in file:
@@ -10,6 +25,17 @@ with open ('receip.txt', encoding = 'utf-8') as file:
             numb = file.readline().strip()
             ingredient_name, quantity, measure = numb.split(' | ')
             ing_dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-        cook_book[dish] = ing_dish
+            cook_book[dish] = ing_dish
         file.readline()
+    pprint.pprint(cook_book)
+
+
+   
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
+
+         
+           
+    
+                 
 
