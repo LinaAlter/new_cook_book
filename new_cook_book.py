0740544ae6f95765712b918f09ed0046e1341e77
Anna Volkova <anotherwaygirl@yandex.ru>
2024-07-19 19:38:54 +0700

    last change in cook_book, junk deleted

--- new_cook_book.py
+++ new_cook_book.py
@@ -1,20 +1,25 @@
 import pprint
-def get_shop_list_by_dishes(dishes, person_count):
+
+    
+def get_shop_list_by_dishes(dishes, person_count):       # функция составляет список покупок по названию блюда и количеству персон
     
     ings_shop_list = {}
     for dish in dishes:
-        for ing_dish in cook_book[dish]:
-            if dish in cook_book:
-           #for ing_dish in cook_book[dish]:
+        if dish in cook_book:     
+            for ing_dish in cook_book[dish]:
+            
                 prod = ing_dish['ingredient_name']
                 ings_shop_list.setdefault(prod, [])
                 ings = int(ing_dish['quantity']) * person_count
-                ing_list = []
-                ing_list.append({'quantity': ings, 'measure': ing_dish['measure']})
-               
-                ings_shop_list[prod] = ing_list
+                v = {'quantity': ings, 'measure': ing_dish['measure']}        
+                ings_shop_list[prod] = v
+        else:
+            print(f'Блюда "{dish}" нет в книге рецептов')                
+    
     pprint.pprint(ings_shop_list)
     return ings_shop_list
+
+
 with open ('receip.txt', encoding = 'utf-8') as file:
     cook_book = {}
     for line in file:
